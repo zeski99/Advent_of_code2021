7179c2fe69dc465275a9913860e8abb162a759a9
zeski <zeski@dragonsec.si>
2021-12-21 14:12:48 +0100

    day21 optimization

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -23,19 +23,17 @@ fn part_two(pos: [i8;2], score: [u32;2], turn: usize, memo: &mut HashMap<([i8;2]
         Some(&ret) => return ret,
         None => ()
     }
+    let mut new_pos: [i8;2] = [0,0];
+    let mut new_score: [u32;2] = [0,0];
     let mut out = [0,0];
-    for d1 in [1,2,3] {
-        for d2 in [1,2,3] {
-            for d3 in [1,2,3] {
-                let mut new_pos = pos;
-                let mut new_score = score;
-                new_pos[turn] = (pos[turn] + d1 + d2 + d3 - 1) % 10 + 1;
-                new_score[turn] += new_pos[turn] as u32;
-                let tmp = part_two(new_pos, new_score, turn^1, memo);
-                out[0] += tmp[0];
-                out[1] += tmp[1];
-            }
-        }
+    for (d,n) in [(3,1), (4,3), (5,6), (6,7), (7,6), (8,3), (9,1)]{
+        new_pos[turn^1] = pos[turn^1];
+        new_pos[turn] = (pos[turn] + d - 1) % 10 + 1;
+        new_score[turn^1] = score[turn^1];
+        new_score[turn] = score[turn^1] + new_pos[turn] as u32;
+        let tmp = part_two(new_pos, new_score, turn^1, memo);
+        out[0] += n*tmp[0];
+        out[1] += n*tmp[1];
     }
     memo.insert((pos, score, turn), out);
     out
